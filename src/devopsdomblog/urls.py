017f2bc2aa5d3ff2bcf9ad8a2ae2ea574a84eb03
DevOpsDom <devopsdom@gmail.com>
2020-09-12 22:34:59 +0100

    finished creating the comments section for authenticated users

--- src/devopsdomblog/urls.py
+++ src/devopsdomblog/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from post.views import index, blog, post, search
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('blog/', blog, name='post_list'),
     path('search/', search, name='search'),
     path('post/<id>/', post, name='post_detail'),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG:
