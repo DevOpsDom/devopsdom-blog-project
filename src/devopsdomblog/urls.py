bd133b33581b6940405230de04acf259df2d28b4
DevOpsDom <devopsdom@gmail.com>
2020-09-13 00:45:36 +0100

    finished adding the custom editor for authenticated users, fixed formatting and display issues, added update and delete buttons on posts for authenticated users

--- src/devopsdomblog/urls.py
+++ src/devopsdomblog/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from post.views import index, blog, post, search
+from post.views import index, blog, post, search, post_update, post_delete, post_create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog, name='post_list'),
     path('search/', search, name='search'),
+    path('create/', post_create, name='post_create'),
+    path('post/<id>/update', post_update, name='post_update'),
+    path('post/<id>/delete', post_delete, name='post_delete'),
     path('post/<id>/', post, name='post_detail'),
     path('tinymce/', include('tinymce.urls')),
 ]
