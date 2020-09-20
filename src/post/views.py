046a4b2e8be1b95fbf0c486961f1ffcc6ce189bf
DevOpsDom <devopsdom@gmail.com>
2020-09-20 22:12:01 +0100

    made changes to the styling and colorscheme

--- src/post/views.py
+++ src/post/views.py
@@ -34,7 +34,7 @@ def get_category_count():
 
 def index(request):
     featured = Post.objects.filter(featured=True)
-    latest = Post.objects.order_by('-timestamp')[0:3]
+    latest = Post.objects.order_by('-timestamp')[0:6]
 
     if request.method == "POST":
         email = request.POST["email"]
